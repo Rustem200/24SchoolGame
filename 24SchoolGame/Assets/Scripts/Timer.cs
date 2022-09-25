378e40bcd0c87c87065b3620f3f647d49d540e04
Rustem200 <roldppol200@gmail.com>
2022-09-25 17:08:52 +0300

    cum - commit

--- 24SchoolGame/Assets/Scripts/Timer.cs
+++ 24SchoolGame/Assets/Scripts/Timer.cs
@@ -6,8 +6,11 @@ using UnityEngine.UI;
 public class Timer : MonoBehaviour
 {
     private float _time;
-    private float _maxTime = 5;
+    private float _maxTime = 50;
     private int _hour;
+    [SerializeField] private GameObject _adolfAndreevich;
+    [SerializeField] private GameObject _skovorodka;
+    [SerializeField] private GameObject _kyzykhaEvil;
     [SerializeField] private Text _text;
     [SerializeField] private SceneController _sceneController;
 
@@ -23,8 +26,15 @@ public class Timer : MonoBehaviour
             _time = 0;
         }
 
+        if (_hour >= 2)
+            _adolfAndreevich.SetActive(true);
+        if (_hour >= 4)
+            _skovorodka.SetActive(true);
+        if (_hour >= 5)
+            _kyzykhaEvil.SetActive(true);
+
         if(_hour > 6)
-            _sceneController.Scenes(1);
+            _sceneController.Scenes(2);
 
         _text.text = _hour + ".AM";
     }
